554484a91bb91210d8441863027cabdce9a04f77
Alexis Burgos (gtm) <34293632+AlexisBurgosGtm@users.noreply.github.com>
2022-05-29 09:15:55 -0600

    s

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ let routerCenso = require('./router/routerCenso');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-const PORT = process.env.PORT || 4412;
+const PORT = process.env.PORT || 4413;
 
 app.use(bodyParser.json());
 
