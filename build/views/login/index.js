4893ca5b8302f31db0084a463b16c956411afa35
Alexis Burgos (gtm) <34293632+AlexisBurgosGtm@users.noreply.github.com>
2022-07-28 00:45:20 -0600

    Update index.js

--- build/views/login/index.js
+++ build/views/login/index.js
@@ -129,16 +129,18 @@ function addListeners(){
     //carga las sucursales directamente desde código
     document.getElementById('cmbSucursal').innerHTML = funciones.getComboSucursales();
 
-    selectDateDownload(); //carga la info inicial
-    try {
-        document.getElementById('cmbSucursal').value = GlobalCodSucursal;
-        console.log(GlobalCodSucursal);
-    } catch (error) {
-        console.log('error al cargar sucursal')
-        console.log(error)
-    }
+    selectDateDownload() //carga la info inicial
+    .then(()=>{
+        try {
+            document.getElementById('cmbSucursal').value = GlobalCodSucursal;
+            console.log(GlobalCodSucursal);
+        } catch (error) {
+            console.log('error al cargar sucursal')
+            console.log(error)
+        }
+    })
+   
   
-
 };
 
 
